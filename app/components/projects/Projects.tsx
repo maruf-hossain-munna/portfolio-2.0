b29b521dfb2a554d6752571dcb13905efd618506
Maruf Hossain Munna <marufhossainmunna21@gmail.com>
2024-03-06 10:21:01 -0800

    added a new project

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -12,25 +12,25 @@ const Projects = () => {
 
             <div className='my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 gap-y-12'>
                 <ProjectsCard
-                    title="Doctor's Portal"
-                    image='/images/web/doctors-portal.png'
-                    category='React website'
-                    description="This is Doctor's Portal website. it has dynamic appointment booking system. Develop with React.js, JS(ES6), TailwindCSS, Node.js(Express.js), Firebase"
-                    gitHubLink='https://github.com/maruf-hossain-munna/doctors-portal-client'
-                    liveLink="https://doctor-portal-8402d.web.app/"
+                    title="Miro"
+                    image='/images/web/miro.png'
+                    category='Next js website'
+                    description="This is a business website converted from figma. Create custom Tabs & Carousel components not using any ui library.using React fast marquee package for auto scrolling animation."
+                    gitHubLink='https://github.com/maruf-hossain-munna/miro'
+                    liveLink="https://miroo-lac.vercel.app/"
                 />
                 <ProjectsCard
                     title="Mired"
                     image='/images/web/mired.png'
-                    category='Next website'
+                    category='Next js website'
                     description="It's just a frontend project. It's a paid work. Develop this design converting figma. Fully pixel perfect design. Develop with Next.js, Typescript, TailwindCSS."
                     gitHubLink='https://github.com/maruf-hossain-munna/mired'
-                    liveLink="https://miredd.vercel.app/"
+                    liveLink="https://mired.vercel.app/"
                 />
                 <ProjectsCard
                     title="E-Shop"
                     image='/images/web/e-shop.png'
-                    category='Next website'
+                    category='Next js website'
                     description="This is an E-commerce website, It's a running project. Completed basic frontend design and working on backend and database. Develop with Next.js, Typescript, TailwindCSS, Next-auth, Prisma, MongoDB."
                     gitHubLink='https://github.com/maruf-hossain-munna/E-Shop'
                     liveLink="https://onlien-e-shop.vercel.app/"
@@ -45,19 +45,27 @@ const Projects = () => {
                 <ProjectsCard
                     title="Frequency Investment"
                     image='/images/web/frequency-Invest.png'
-                    category='Next website'
+                    category='Next js website'
                     description="This is a landing page. Converted from Figma design to Next.js website. Develop with Next.js(13), Typescript, TailwindCSS"
                     gitHubLink='https://github.com/maruf-hossain-munna/frequency-invest'
                     liveLink="https://frequency-invest.vercel.app/"
                 />
                 <ProjectsCard
+                    title="Doctor's Portal"
+                    image='/images/web/doctors-portal.png'
+                    category='React website'
+                    description="This is Doctor's Portal website. it has dynamic appointment booking system. Develop with React.js, JS(ES6), TailwindCSS, Node.js(Express.js), Firebase"
+                    gitHubLink='https://github.com/maruf-hossain-munna/doctors-portal-client'
+                    liveLink="https://doctor-portal-8402d.web.app/"
+                />
+                {/* <ProjectsCard
                     title="Learn Confidently"
                     image='/images/web/learn-confidently.png'
                     category='React website'
                     description="This is an educational website (LMS). It's just designed frontend. Not use payment processing system. Develop with React.js, tailwind, daisy ui"
                     gitHubLink='https://github.com/maruf-hossain-munna/learn-confidently'
                     liveLink="https://learn-confidently.netlify.app/"
-                />
+                /> */}
                 {/* <ProjectsCard
                     title="TLC Roofing"
                     image='/images/web/tlc-roofing.png'
